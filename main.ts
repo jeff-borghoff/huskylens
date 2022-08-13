2ae761e90a5a21202128245ef1753f3e99ecb3f1
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2022-08-13 17:24:34 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isLearned(1)) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            serial.writeValue("box", huskylens.readeBox(1, Content1.xCenter))
+        }
+    }
+})
